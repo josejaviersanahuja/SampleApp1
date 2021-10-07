91ec8ffd0c0e27416b59a66c2f94fb8bd2d2a1f1
Jose Javier Sanahuja <josejaviersanahuja@gmail.com>
2021-10-07 10:36:23 +0100

    implementamos cardView y glide. empezamos la util para simular nuestra base de datos de libros

--- app/src/main/java/com/zitrojjdev/sampleapp1/AllBooksRecyclerViewAdapter.java
+++ app/src/main/java/com/zitrojjdev/sampleapp1/AllBooksRecyclerViewAdapter.java
@@ -10,7 +10,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
@@ -20,6 +23,7 @@ public class AllBooksRecyclerViewAdapter extends RecyclerView.Adapter<AllBooksRe
     // fields of this class
     private ArrayList<Book> listOfBooks = new ArrayList<>();
     private Context context;
+
 
     public AllBooksRecyclerViewAdapter(Context context) {
         this.context = context;
@@ -31,7 +35,8 @@ public class AllBooksRecyclerViewAdapter extends RecyclerView.Adapter<AllBooksRe
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
     // 99% of the times, this will be the code needed.
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.all_books_recycler_view, null);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.all_books_recycler_view,null);
+        // View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.all_books_recycler_view, parent, false);
         ViewHolder holder = new ViewHolder(view);
         return holder;
     }
@@ -47,9 +52,24 @@ public class AllBooksRecyclerViewAdapter extends RecyclerView.Adapter<AllBooksRe
             @Override
             public void onClick(View view) {
                 final int index = holder.getAdapterPosition();
+                Toast.makeText(context, listOfBooks.get(index).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // nuevo onclick listener
+        holder.bookCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final int index = holder.getAdapterPosition();
                 Toast.makeText(context, listOfBooks.get(index).toString(), Toast.LENGTH_SHORT).show();
             }
         });
+
+        // Aquí añadimos la url Image usando una librería externa con Glide
+        Glide.with(context)
+                .asBitmap()
+                .load(listOfBooks.get(position).getImageURL())
+                .into(holder.bookImage);
     }
 
     // just passing the size od the data array
@@ -62,12 +82,14 @@ public class AllBooksRecyclerViewAdapter extends RecyclerView.Adapter<AllBooksRe
 
         private ImageView bookImage;
         private TextView bookName;
+        private CardView bookCard;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             bookImage = itemView.findViewById(R.id.imgBook);
             bookName = itemView.findViewById(R.id.titleBook);
+            bookCard = itemView.findViewById(R.id.bookCard);
 
         }
     }
